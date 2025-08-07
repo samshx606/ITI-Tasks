2b56230d64b78128a7669fcdbe10b76a2ed0d3c4
samshx606 <saaameh.0.1@gamil.com>
2025-08-07 20:48:18 +0300

    modified Day2

--- Angular/Day2/lab2/my-app/src/app/components/card-list/card-list.ts
+++ Angular/Day2/lab2/my-app/src/app/components/card-list/card-list.ts
@@ -22,13 +22,11 @@ export class CardList {
       bio: user.bio
     }));
     this.cards = [...this.allCards];
-    this.updateLastCard();
   }
   onRemoveCard(id: number) {
     this.userService.deleteUser(id);
     this.allCards = this.allCards.filter(card => card.id !== id);
     this.cards = this.cards.filter(card => card.id !== id);
-    this.updateLastCard();
   }
 
   onSearchChange(searchTerm: string) {
@@ -40,15 +38,6 @@ export class CardList {
     } else {
       this.cards = this.allCards.filter(card =>
         card.name.toLowerCase().startsWith(searchTerm.toLowerCase()));
-    }
-    if(this.cards[this.cards.length-1].isLast == false)
-      this.cards[this.cards.length-1].isLast = true;
-    this.updateLastCard();
-  }
-
-  private updateLastCard() {
-    if(this.cards.length > 0 && !this.cards[this.cards.length - 1]) {
-      this.cards[this.cards.length - 1].isLast = true;
     }
   }
 }
